bb83d2b3616a6b8d99d43be4c283ed38699c4b0d
DavideBernardini <davidebernardini09@gmail.com>
2021-08-25 15:46:45 +0200

    Inizio esercizio

---
+++ snack-5/script.js
@@ -0,0 +1,9 @@
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+
+// array vuoto
+var numeri = [];
+var numeroscelto
+// chiedere per 6 volte all’utente di inserire un numero
+for (var i; i < 6; i++) {
+   numeroscelto = prompt("Inserisci un numero")
+}
